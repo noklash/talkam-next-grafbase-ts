5a80833446a87e3416685823abef15edde2a0642
noklash <123834419+noklash@users.noreply.github.com>
2023-09-01 11:55:03 +0300

    saved dev for jsonwebtoken to remove bug

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
+import { JWT } from 'next-auth/jwt';
+import jsonwebtoken from "jsonwebtoken";
 import GitHubProvider from 'next-auth/providers/github';
+
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -7,7 +10,37 @@ export const authOptions: NextAuthOptions = {
           clientId: process.env.GITHUB_CLIENT_ID!,
           clientSecret: process.env.GITHUB_CLIENT_SECRET!
         })
-      ]
+      ],
+//    To finish configuring NextAuth.js with Grafbase we will need to update the JWT
+//     generated to include the iss claim that matches the issuer value in
+//      the grafbase/schema.graphql config.
+jwt: {
+    encode: ({ secret, token }) =>
+      jsonwebtoken.sign(
+        {
+          ...token,
+          iss: 'nextauth',
+          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 60
+        },
+        secret
+      ),
+    decode: async ({ secret, token }) =>
+      jsonwebtoken.verify(token!, secret) as JWT
+  },
+  callbacks: {
+    async jwt({ token, profile }) {
+      if (profile) {
+        token.username = profile?.login
+      }
+      return token
+    },
+    session({ session, token }) {
+      if (token.username) {
+        session.username = token?.username
+      }
+      return session
+    }
+  }
 }
 
 export default NextAuth(authOptions)
