c28de4f4773cedc8d051fd0ee1d7f587d01f5b3a
noklash <123834419+noklash@users.noreply.github.com>
2023-09-04 09:43:28 +0300

    switched the assignment from rleft to right and vice versa

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,10 @@ import { JWT } from 'next-auth/jwt';
 import jsonwebtoken from "jsonwebtoken";
 import GitHubProvider from 'next-auth/providers/github';
 
+import { getServerSession } from "next-auth/next";
+import { AdapterUser } from "next-auth/adapters";
+// import { createUser, getUser } from "./actions";
+
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -36,9 +40,10 @@ jwt: {
     },
     session({ session, token }) {
       if (token.username) {
-        // const sessionInfo = session?.user?.name
+        
         // if (session.user !== undefined){
-          session.username = token?.username
+          token.username = session.user?.name 
+          // still trying 
           // console.log(session.user)
         // }
         
@@ -49,3 +54,75 @@ jwt: {
 }
 
 export default NextAuth(authOptions)
+
+
+// New here
+// jwt: {
+//   encode: ({ secret, token }) => {
+//     const encodedToken = jsonwebtoken.sign(
+//       {
+//         ...token,
+//         iss: "grafbase",
+//         exp: Math.floor(Date.now() / 1000) + 60 * 60,
+//       },
+//       secret
+//     );
+    
+//     return encodedToken;
+//   },
+//   decode: async ({ secret, token }) => {
+//     const decodedToken = jsonwebtoken.verify(token!, secret);
+//     return decodedToken as JWT;
+//   },
+// },
+// theme: {
+//   colorScheme: "light",
+//   logo: "/logo.svg",
+// },
+
+
+// callbacks: {
+//   async session({ session }) {
+//     const email = session?.user?.email as string;
+
+//     try { 
+//       const data = await getUser(email) as { user?: UserProfile }
+
+//       const newSession = {
+//         ...session,
+//         user: {
+//           ...session.user,
+//           ...data?.user,
+//         },
+//       };
+
+//       return newSession;
+//     } catch (error: any) {
+//       console.error("Error retrieving user data: ", error.message);
+//       return session;
+//     }
+//   },
+//   async signIn({ user }: {
+//     user: AdapterUser | User
+//   }) {
+//     try {
+//       const userExists = await getUser(user?.email as string) as { user?: UserProfile }
+      
+//       if (!userExists.user) {
+//         await createUser(user.name as string, user.email as string, user.image as string)
+//       }
+
+//       return true;
+//     } catch (error: any) {
+//       console.log("Error checking if user exists: ", error.message);
+//       return false;
+//     }
+//   },
+// },
+// };
+
+// export async function getCurrentUser() {
+// const session = await getServerSession(authOptions) as SessionInterface;
+
+// return session;
+// }
