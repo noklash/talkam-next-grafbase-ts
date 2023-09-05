172157a958ee899f55bc681bc6206d6be9f61a9a
noklash <123834419+noklash@users.noreply.github.com>
2023-09-05 22:44:54 +0300

    error on button click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,42 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Header } from '@/components/header'
+import { MessageList } from '@/components/message-list'
+import { NewMessageForm } from '@/components/new-message-form'
+import { useSession } from 'next-auth/react'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session } = useSession()
   return (
     <div className="flex flex-col">
-      <h1 className="text-white">Hello Grafbase!</h1>
+      <Header />
+      {session ? (
+        <>
+        <div className='flex-1 overflow-y-scroll p-6'>
+          <div className='max-w-4xl mx-auto'>
+            <div className='flex justify-between items-center'>
+              <MessageList/>
+            </div>
+          </div>
+        </div>
+        {/* </> */}
+        <div className="p-6 bg-white/5 border-t border-[#363739]">
+            <div className="max-w-4xl mx-auto">
+              <NewMessageForm />
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="h-full flex items-center justify-center">
+          <p className="text-lg md:text-2xl text-white">
+            Sign in to join the chat!
+          </p>
+        </div>
+      )}
+      
     </div>
   )
 }
