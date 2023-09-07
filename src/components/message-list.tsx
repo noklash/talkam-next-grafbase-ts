880005f37c835cb81769847783e491a72e81e1c9
noklash <123834419+noklash@users.noreply.github.com>
2023-09-07 13:32:54 +0300

    fixed some databse fetch

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -35,10 +35,10 @@ export const MessageList = () => {
 })
 
 useEffect(() =>{
-    if (inView){
-        entry?.target?.scrollIntoView({ behavior: 'auto' })
+    if (entry?.target){
+        entry?.target?.scrollIntoView({ behavior: 'smooth', block: "end" });
     }
-}, [data, entry, inView])
+}, [data?.messageCollection.edges.length, entry?.target])
 
 if (loading)
     return (
@@ -56,11 +56,11 @@ if (error)
 
 return (
     <div className='flex flex-col space-y-3 overflow-y-scroll w-full'>
-         {!inView && (
+         {!inView && data?.messageCollection.edges.length && (
         <div className="py-1.5 w-full px-3 z-10 text-xs absolute flex justify-center bottom-0 mb-[120px] inset-x-0">
           <button
             className="py-1.5 px-3 text-xs bg-[#1c1c1f] border border-[#363739] rounded-full text-white font-medium"
-            onClick={() => entry?.target?.scrollIntoView({ behavior: 'auto' })}
+            onClick={() => entry?.target?.scrollIntoView({ behavior: 'smooth', block: "end" })}
           >
             Scroll to see the latest messages
           </button>
